0a752595c10a1113b8098c91dc7cd2a97c2fe469
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-01-15 13:57:36 +0200

    scroll not working

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,94 +35,99 @@ class _MyAppState extends State<MyApp> {
           children: [
             // PdfViewer.asset(
             //   'assets/hello.pdf',
-            PdfViewer.uri(
-              Uri.parse(kIsWeb
-                  ? 'assets/assets/hello.pdf'
-                  : 'https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf'),
-              controller: controller,
-              displayParams: PdfViewerParams(
-                maxScale: 8,
-                // code to display pages horizontally
-                // layoutPages: (pages, params) {
-                //   final height = pages.fold(
-                //           templatePage.height,
-                //           (prev, page) => max(prev, page.height)) +
-                //       params.margin * 2;
-                //   final pageLayouts = <Rect>[];
-                //   double x = params.margin;
-                //   for (var page in pages) {
-                //     page ??= templatePage; // in case the page is not loaded yet
-                //     pageLayouts.add(
-                //       Rect.fromLTWH(
-                //         x,
-                //         (height - page.height) / 2, // center vertically
-                //         page.width,
-                //         page.height,
-                //       ),
-                //     );
-                //     x += page.width + params.margin;
-                //   }
-                //   return PdfPageLayout(
-                //     pageLayouts: pageLayouts,
-                //     documentSize: Size(x, height),
-                //   );
-                // },
-                //
-                // Scroll-thumbs example
-                //
-                viewerOverlayBuilder: (context, size) => [
-                  // Show vertical scroll thumb on the right; it has page number on it
-                  PdfViewerScrollThumb(
-                    controller: controller,
-                    orientation: ScrollbarOrientation.right,
-                    thumbSize: const Size(40, 25),
-                    thumbBuilder:
-                        (context, thumbSize, pageNumber, controller) =>
-                            Container(
-                      color: Colors.black,
-                      child: Center(
-                        child: Text(
-                          pageNumber.toString(),
-                          style: const TextStyle(color: Colors.white),
+            SingleChildScrollView(
+              child: SizedBox(
+                height: 300,
+                child: PdfViewer.uri(
+                  Uri.parse(kIsWeb
+                      ? 'assets/assets/hello.pdf'
+                      : 'https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf'),
+                  controller: controller,
+                  displayParams: PdfViewerParams(
+                    maxScale: 8,
+                    // code to display pages horizontally
+                    // layoutPages: (pages, params) {
+                    //   final height = pages.fold(
+                    //           templatePage.height,
+                    //           (prev, page) => max(prev, page.height)) +
+                    //       params.margin * 2;
+                    //   final pageLayouts = <Rect>[];
+                    //   double x = params.margin;
+                    //   for (var page in pages) {
+                    //     page ??= templatePage; // in case the page is not loaded yet
+                    //     pageLayouts.add(
+                    //       Rect.fromLTWH(
+                    //         x,
+                    //         (height - page.height) / 2, // center vertically
+                    //         page.width,
+                    //         page.height,
+                    //       ),
+                    //     );
+                    //     x += page.width + params.margin;
+                    //   }
+                    //   return PdfPageLayout(
+                    //     pageLayouts: pageLayouts,
+                    //     documentSize: Size(x, height),
+                    //   );
+                    // },
+                    //
+                    // Scroll-thumbs example
+                    //
+                    viewerOverlayBuilder: (context, size) => [
+                      // Show vertical scroll thumb on the right; it has page number on it
+                      PdfViewerScrollThumb(
+                        controller: controller,
+                        orientation: ScrollbarOrientation.right,
+                        thumbSize: const Size(40, 25),
+                        thumbBuilder:
+                            (context, thumbSize, pageNumber, controller) =>
+                                Container(
+                          color: Colors.black,
+                          child: Center(
+                            child: Text(
+                              pageNumber.toString(),
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ),
                         ),
                       ),
+                      // Just a simple horizontal scroll thumb on the bottom
+                      PdfViewerScrollThumb(
+                        controller: controller,
+                        orientation: ScrollbarOrientation.bottom,
+                        thumbSize: const Size(80, 30),
+                        thumbBuilder:
+                            (context, thumbSize, pageNumber, controller) =>
+                                Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                    //
+                    // Loading progress indicator example
+                    //
+                    loadingBannerBuilder:
+                        (context, bytesDownloaded, totalBytes) => Center(
+                      child: CircularProgressIndicator(
+                        value: totalBytes != null
+                            ? bytesDownloaded / totalBytes
+                            : null,
+                        backgroundColor: Colors.grey,
+                      ),
                     ),
-                  ),
-                  // Just a simple horizontal scroll thumb on the bottom
-                  PdfViewerScrollThumb(
-                    controller: controller,
-                    orientation: ScrollbarOrientation.bottom,
-                    thumbSize: const Size(80, 30),
-                    thumbBuilder:
-                        (context, thumbSize, pageNumber, controller) =>
-                            Container(
-                      color: Colors.red,
+                    //
+                    // Link handling example
+                    //
+                    // FIXME: a link with several areas (link that contains line-break) does not correctly
+                    // show the hover status
+                    // FIXME: gestures other than tap should be passed-through to the underlying widget
+                    linkWidgetBuilder: (context, link, size) => Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () => print('link tapped: ${link.url}'),
+                        hoverColor: Colors.blue.withOpacity(0.2),
+                      ),
                     ),
-                  ),
-                ],
-                //
-                // Loading progress indicator example
-                //
-                loadingBannerBuilder: (context, bytesDownloaded, totalBytes) =>
-                    Center(
-                  child: CircularProgressIndicator(
-                    value: totalBytes != null
-                        ? bytesDownloaded / totalBytes
-                        : null,
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-                //
-                // Link handling example
-                //
-                // FIXME: a link with several areas (link that contains line-break) does not correctly
-                // show the hover status
-                // FIXME: gestures other than tap should be passed-through to the underlying widget
-                linkWidgetBuilder: (context, link, size) => Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    onTap: () => print('link tapped: ${link.url}'),
-                    hoverColor: Colors.blue.withOpacity(0.2),
                   ),
                 ),
               ),
